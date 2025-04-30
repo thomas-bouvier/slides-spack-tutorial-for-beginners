47dbfa61710299f40305f0fbe3b70594eb568981
Fernando Ayats <ayatsfer@gmail.com>
2025-04-30 12:18:52 +0200

    Add pdf

--- shell.nix
+++ shell.nix
@@ -1,7 +1,14 @@
 with import <nixpkgs> { };
 mkShell {
   packages = [
-    (nodejs.override {enableNpm=false;})
+    # (nodejs.override { enableNpm = false; })
+    nodejs
     corepack
+    playwright-driver.browsers
   ];
+
+  env = {
+    PLAYWRIGHT_BROWSERS_PATH = pkgs.playwright-driver.browsers;
+    PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS = "true";
+  };
 }
